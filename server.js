3093943846ed4e01f2d07b7b80e43be1205ff0c9
vishal181214 <vishal32gaykwad@gmail.com>
2022-11-11 00:17:56 +0530

    cors installed

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const data1 = require("./Routes/bollywood")
+const cors = require('cors')
 
+app.use(cors);
 app.use('/home', data1)
 
 app.listen(process.env.PORT||4000,()=>{
