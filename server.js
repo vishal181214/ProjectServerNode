6287083f38686a5e6d2cc5c9b60cfe478b105aca
vishal181214 <vishal32gaykwad@gmail.com>
2022-11-11 11:36:39 +0530

    cors are updated with port

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const data1 = require("./Routes/bollywood")
 const cors = require('cors')
+// const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
-app.use(cors);
+app.use(cors(corsOptions))
+// app.use(cors);
 app.use('/home', data1)
 
-app.listen(process.env.PORT||8400,()=>{
+app.listen(process.env.PORT||8080,()=>{
     console.log("server running");
 })
